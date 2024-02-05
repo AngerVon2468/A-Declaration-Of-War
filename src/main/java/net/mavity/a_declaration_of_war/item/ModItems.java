3823c04885fb7672a9e829aea4507a264a6b842e
AngerVon2468 <thomaschad0909@gmail.com>
2024-02-06 09:21:36 +1100

    Made the Knife a SwordItem of Tier Iron
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/a_declaration_of_war/item/ModItems.java
+++ src/main/java/net/mavity/a_declaration_of_war/item/ModItems.java
@@ -3,6 +3,7 @@ package net.mavity.a_declaration_of_war.item;
 import net.mavity.a_declaration_of_war.a_declaration_of_war;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tiers;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -12,7 +13,8 @@ public class ModItems {
             DeferredRegister.createItems(a_declaration_of_war.MODID);
 
     public static final DeferredItem<Item> A_Knife = ITEMS.register("a_knife",
-            () -> new Item(new Item.Properties()));
+            () -> new SwordItem(Tiers.IRON, 2, -2.4f,
+                    new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
