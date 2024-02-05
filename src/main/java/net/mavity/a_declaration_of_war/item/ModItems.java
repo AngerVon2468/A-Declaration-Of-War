4fb4ce414beeb1c09b32138e8f79267448ae3360
AngerVon2468 <thomaschad0909@gmail.com>
2024-02-06 08:44:00 +1100

    Tried to make new item, failed so deleted
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/a_declaration_of_war/item/ModItems.java
+++ src/main/java/net/mavity/a_declaration_of_war/item/ModItems.java
@@ -2,6 +2,7 @@ package net.mavity.a_declaration_of_war.item;
 
 import net.mavity.a_declaration_of_war.a_declaration_of_war;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -12,6 +13,7 @@ public class ModItems {
 
     public static final DeferredItem<Item> A_Knife = ITEMS.register("a_knife",
             () -> new Item(new Item.Properties()));
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
